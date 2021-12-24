0ffb6a9641db0e1f2df89686ab45b209181fc9ad
Josh Wong <joshwong74@gmail.com>
2021-12-23 19:27:17 -0800

    Shelf items work, app icons updated, bouncing shelf cubes

--- src/components/shelfarchive.js
+++ src/components/shelfarchive.js
@@ -16,12 +16,12 @@ class Shelf extends Component {
     this.state = {
       showPlane: false,
       fade: 0,
-      linkedin: false,
+      linkedin: {
+        hovering: false,
+        animating: false,
+      },
       github: false,
       gmail: false,
-      linkedinAnimation: false,
-      githubAnimation: false,
-      gmailAnimation: false,
     }
   }
   handleClick(target) {
@@ -40,13 +40,43 @@ class Shelf extends Component {
     }
   }
 
-  handleEvent(type, bool=true) {
+  handleEvent(type, bool=true, property=null) {
     switch(type) {
       case "animation end":
         this.setState({ fade: false })
         break;
       case "linkedin":
-        this.setState({ "linkedin": bool })
+        if (property == "hovering") {
+          if (bool) {
+          this.setState({
+            linkedin: {
+              hovering: true
+            }
+          })
+          } else {
+            this.setState({
+              linkedin: {
+                hovering: false
+              }
+            })
+          }
+        } else if (property == "animating") {
+          // alert(property + " is animating" + bool)
+          if (bool) {
+            this.setState({
+              linkedin: {
+                animating: true
+              }
+            })
+            } else {
+              this.setState({
+                linkedin: {
+                  animating: false
+                }
+              })
+            }
+        }
+        
         break;
       case "github":
         this.setState({ "github": bool })
@@ -56,13 +86,6 @@ class Shelf extends Component {
         break;
       case "linkedinAnimation":
         this.setState({ "linkedinAnimation": bool })
-        break;
-      case "githubAnimation":
-        this.setState({ "githubAnimation": bool })
-        break;
-      case "gmailAnimation":
-        this.setState({ "gmailAnimation": bool })
-        break;
     }
   }
 
@@ -74,10 +97,10 @@ class Shelf extends Component {
           alt={altname}
           className={`shelf-item clickable static ${name} ${state ? "bounce" : ""}`}
           onClick={() => this.handleClick(name)}
-          onMouseOver={() => this.handleEvent(name, true)}
-          onMouseOut={() => this.handleEvent(name, false)}
-          onAnimationStart={() => this.handleEvent(name + "Animation", true)}
-          onAnimationEnd={() => this.handleEvent(name + "Animation", false)}
+          onMouseOver={() => this.handleEvent(name, true, "hovering")}
+          onMouseOut={() => this.handleEvent(name, false, "hovering")}
+          onAnimationStart={() => this.handleEvent(name, true, "animating")}
+          onAnimationEnd={() => this.handleEvent(name, false, "animating")}
           >
         </img>
       );
@@ -92,8 +115,6 @@ class Shelf extends Component {
           onClick={() => this.handleClick(name)}
           onMouseOver={e => (this.handleEvent(name, true), e.currentTarget.src = gmail_clipboard_bold_img)}
           onMouseOut={e => (this.handleEvent(name, false), e.currentTarget.src = gmail_img)}
-          onAnimationStart={() => this.handleEvent(name + "Animation", true)}
-          onAnimationEnd={() => this.handleEvent(name + "Animation", false)}
           >
         </img>
       );
@@ -108,9 +129,10 @@ class Shelf extends Component {
             <img src={shelf_img} alt="Shelf" className="shelf"></img>
             <img src={books_img} alt="Books" className="shelf-item books"></img>
             <img src={plane_img} alt="Plane" className="shelf-item clickable plane" onClick={this.props.clickHandler}></img>
-            {renderBoxItem("linkedin", "LinkedIn", linkedin_img, this.state.linkedin || this.state.linkedinAnimation)}
-            {renderBoxItem("github", "GitHub", github_img, this.state.github || this.state.githubAnimation)}
-            {renderHoverBoxItem("gmail", "Gmail", gmail_img, this.state.gmail || this.state.gmailAnimation)}
+            {console.log(this.state.linkedin["animating"])}
+            {renderBoxItem("linkedin", "LinkedIn", linkedin_img, (this.state.linkedin["hovering"] || this.state.linkedin["hovering"] == null) || (this.state.linkedin["animating"] || this.state.linkedin["animating"] == null))}
+            {renderBoxItem("github", "GitHub", github_img, this.state.github)}
+            {renderHoverBoxItem("gmail", "Gmail", gmail_img, this.state.gmail)}
             {this.state.fade ? <img src={clipboard_message_img} alt="Clipboard Message" className="shelf-item message" onAnimationEnd={() => this.handleEvent("animation end")} fade={this.state.fade}></img> : null}
         </div>
     );

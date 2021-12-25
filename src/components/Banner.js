a3f99d63cef200e302002e55f8db81e29f5a90e8
Josh Wong <joshwong74@gmail.com>
2021-12-25 00:56:43 -0800

    Added banner animation and click redirection

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import banner_img from "../img/Banner.png";
 
 class Banner extends Component {
+  handleClick() {
+    window.open("https://www.calpoly.edu/", "_blank");
+  }
   render() {
     return (
-        <div>
-            <img src={banner_img} alt="Banner" className="banner"></img>
-        </div>
+      <img src={banner_img} alt="Banner" className="banner clickable" onClick={() => this.handleClick()}></img>
     );
   }
 }

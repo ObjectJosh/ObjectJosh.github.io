023457eb908462d07d9825ecfa4c8be5c074e659
Josh Wong <joshwong74@gmail.com>
2021-12-25 02:11:13 -0800

    Improved banner animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import banner_img from "../img/Banner.png";
 
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      animating: false
+    }
+  }
   handleClick() {
     window.open("https://www.calpoly.edu/", "_blank");
   }
   render() {
     return (
-      <img src={banner_img} alt="Banner" className="banner clickable" onClick={() => this.handleClick()}></img>
+      <img src={banner_img} alt="Banner"
+        className={`banner clickable ${this.state.animating ? "tilt" : ""}`}
+        onClick={() => this.handleClick()}
+        onMouseOver={() => this.setState({ animating: true })}
+        onAnimationEnd={() => this.setState({ animating: false })}
+        >
+      </img>
     );
   }
 }
